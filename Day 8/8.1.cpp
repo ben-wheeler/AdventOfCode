c361fc256a5cb668b054326130a06e838e57ea13
Ben <92675290+ben-wheeler@users.noreply.github.com>
2021-12-22 17:43:24 +1030

    Update 8.1.cpp

--- Day 8/8.1.cpp
+++ Day 8/8.1.cpp
@@ -6,6 +6,17 @@
 
 using namespace std;
 
+struct 
+ display{
+    string wires;
+    string output;
+    display(string one, string two){
+        wires = one;
+        output = two;
+    }
+};
+
+
 void print(vector<vector<int> > input)
 {
     for (int i = 0; i < input.size(); i++)
@@ -30,25 +41,26 @@ int main()
 {
     string line;
 
-    vector<int> input;
     getline(cin, line);
     replace(line.begin(), line.end(), ',', ' ');
     istringstream first_line(line);
     string first_a;
+
+    string delimiter = "|";
+    vector<display> input;
+
     while (first_line >> first_a)
     {
-        if (!first_a.empty())
-        {
-            input.push_back(stoi(first_a));
-        }
+        display temp((first_a.substr(0, first_a.find(delimiter))),(first_a.substr(first_a.find(delimiter),0)));
+       
     }
 
     sort(input.begin(), input.end());
 
     // print(input);
     // cout << endl;
-    cout << input[input.size()/2] << " ";
-    int fuelCount = 0;
+    // cout << input[input.size()/2] << " ";
+    // int fuelCount = 0;
 
     for(int i = 0; i < input.size(); i++){
 
