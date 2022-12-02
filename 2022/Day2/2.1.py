a526063b132fd050b2b1bcd53135699a443b83f5
Ben <92675290+ben-wheeler@users.noreply.github.com>
2022-12-02 22:45:00 +1030

    final solution

--- 2022/Day2/2.1.py
+++ 2022/Day2/2.1.py
@@ -1,16 +1,61 @@
+def switch(letter):
+    if(letter == 'A'):
+        return 1;
+    elif(letter == 'B'):
+        return 2;
+    elif(letter == 'C'):
+        return 3
+
+def stich(letter):
+    if(letter == 'X'):
+        return 4;
+    elif(letter == 'Y'):
+        return 5;
+    elif(letter == 'Z'):
+        return 6
+
 def main():
        f = open("input1.txt","r")
        Lines = f.readlines()
-       bestSoFar = 0
-       comparator = 0
+       score = 0
+       theirPlay = 0
        for line in Lines:
-            if(line.strip()):
-                comparator += int(line)
-            else:
-                if(comparator > bestSoFar):
-                    bestSoFar = comparator
-                comparator = 0
-       print(bestSoFar)
+            theirPlay = 0
+            yourPlay = 0
+            theirPlay = switch(line[0])
+            yourPlay = stich(line[2])
+            if(theirPlay == 1):
+                if(yourPlay == 4):
+                    score += 3
+                    score += 1
+                elif(yourPlay == 5):
+                    score += 6
+                    score += 2
+                elif(yourPlay == 6):
+                    score += 0
+                    score += 3
+            if(theirPlay == 2):
+                if(yourPlay == 4):
+                    score += 0
+                    score += 1
+                elif(yourPlay == 5):
+                    score += 3
+                    score += 2
+                elif(yourPlay == 6):
+                    score += 6
+                    score += 3      
+            if(theirPlay == 3):
+                if(yourPlay == 4):
+                    score += 6
+                    score += 1
+                elif(yourPlay == 5):
+                    score += 0
+                    score += 2
+                elif(yourPlay == 6):
+                    score += 3
+                    score += 3    
+            
+       print(score)
 
 if __name__ == "__main__":
     main()
--- 2022/Day2/2.2.py
+++ 2022/Day2/2.2.py
@@ -1,25 +1,60 @@
+def switch(letter):
+    if(letter == 'A'):
+        return 1;
+    elif(letter == 'B'):
+        return 2;
+    elif(letter == 'C'):
+        return 3
+
+def stich(letter):
+    if(letter == 'X'):
+        return 4;
+    elif(letter == 'Y'):
+        return 5;
+    elif(letter == 'Z'):
+        return 6
+
 def main():
        f = open("input1.txt","r")
        Lines = f.readlines()
-       bestSoFar = 0
-       secondBest = 0
-       thirdBest = 0
-       comparator = 0
+       score = 0
+       theirPlay = 0
        for line in Lines:
-            if(line.strip()):
-                comparator += int(line)
-            else:
-                if(comparator >= bestSoFar):
-                    thirdBest = secondBest
-                    secondBest = bestSoFar
-                    bestSoFar = comparator
-                elif(comparator >= secondBest):
-                    thirdBest = secondBest
-                    secondBest = comparator
-                elif(comparator >= thirdBest):
-                    thirdBest = comparator
-                comparator = 0
-       print(bestSoFar+secondBest+thirdBest)
+            theirPlay = 0
+            yourPlay = 0
+            theirPlay = switch(line[0])
+            yourPlay = stich(line[2])
+            if(theirPlay == 1):
+                if(yourPlay == 4):
+                    score += 3
+                    score += 0
+                elif(yourPlay == 5):
+                    score += 1
+                    score += 3
+                elif(yourPlay == 6):
+                    score += 2
+                    score += 6
+            if(theirPlay == 2):
+                if(yourPlay == 4):
+                    score += 1
+                    score += 0
+                elif(yourPlay == 5):
+                    score += 2
+                    score += 3
+                elif(yourPlay == 6):
+                    score += 3
+                    score += 6      
+            if(theirPlay == 3):
+                if(yourPlay == 4):
+                    score += 2
+                    score += 0
+                elif(yourPlay == 5):
+                    score += 3
+                    score += 3
+                elif(yourPlay == 6):
+                    score += 1
+                    score += 6    
+       print(score)
 
 if __name__ == "__main__":
     main()
