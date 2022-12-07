26878fcf99f13ab75a012311bacb38c0e8c523bc
Ben <92675290+ben-wheeler@users.noreply.github.com>
2022-12-07 19:44:07 +1030

    competed day 6
    fixed bug with original code

--- 2022/Day6/6.1.py
+++ 2022/Day6/6.1.py
@@ -1,20 +1,12 @@
 def main():
     f = open("input1.txt","r")
     Lines = f.readlines()
-    my_set = set()
-    score = 0
     for line in Lines:
-        for char in line:
-            if(len(my_set) < 4):
-                if char in my_set:
-                    my_set.clear()
-                else:
-                    my_set.add(char)
-            if(len(my_set) == 4):
-                print(score)
+        for i in range(3, len(line)):
+            my_set = set(line[(i-4):i])
+            if len(my_set) == 4:
+                print(i)
                 return
-            score += 1
-
 
 if __name__ == "__main__":
     main()
--- 2022/Day6/6.2.py
+++ 2022/Day6/6.2.py
@@ -1,20 +1,12 @@
 def main():
     f = open("input1.txt","r")
     Lines = f.readlines()
-    my_set = set()
-    score = 0
     for line in Lines:
-        for char in line:
-            if(len(my_set) < 14):
-                if char in my_set:
-                    my_set.clear()
-                else:
-                    my_set.add(char)
-            if(len(my_set) == 14):
-                print(score)
+        for i in range(14, len(line)):
+            my_set = set(line[(i-14):i])
+            if len(my_set) == 14:
+                print(i)
                 return
-            score += 1
-
-
+    
 if __name__ == "__main__":
     main()
