dc596257f78faf1dc458aea91162bf5c98ea4907
Ben <92675290+ben-wheeler@users.noreply.github.com>
2022-12-12 19:02:41 +1030

    current progress

--- 2022/Day7/7.1.py
+++ 2022/Day7/7.1.py
@@ -1,16 +1,18 @@
 class fileStats:
-  def __init__(self, n, s, kids):
-    self.name = n
-    self.size = s
-    self.children = kids
-def __str__(self):
-    # print here
+    def __init__(self, n, s, kids):
+        self.name = n
+        self.size = s
+        self.children = kids
+    def __lt__(self, other):
+        return self.name < other.name
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
 
 def goUp(folder):
     uppedFolder = folder
     while(uppedFolder[-1] != "/"):
         uppedFolder = uppedFolder[:-1]
-    uppedFolder = uppedFolder.rstrip()
+    uppedFolder = uppedFolder[:-1]
     return uppedFolder
 
 def main():
@@ -19,24 +21,29 @@ def main():
     currentFolder = ""
     items = []
     noKids = []
+    kids = []
 
     for line in Lines:
         words = line.split(" ")
         if words[0] == "$":
-            match words[1]:
-                case "cd":
-                    if(words[2] == ".."):
-                        currentFolder = goUp(currentFolder)
+            if words[1] == "cd":
+                words[2] = words[2].strip()
+                if(words[2] == ".."):
+                    currentFolder = goUp(currentFolder)
+                else:
+                    if(words[2] == '/'):
+                        currentFolder += '~'
                     else:
-                        currentFolder += "/"
-                        currentFolder += words[1]
-                case "ls":
-                    break
+                        currentFolder += '/' + words[2]
         elif words[0] == "dir":
-            items.append(fileStats(currentFolder+"/"+words[1],0,noKids))                       
+            words[1] = words[1].strip()
+            items.append(fileStats(currentFolder+"/"+words[1]+"-",0,noKids)) 
+            # get kids                      
         elif words[0].isnumeric():
+            words[1] = words[1].strip()
             items.append(fileStats(currentFolder+"/"+words[1],words[0],noKids))                      
-
+    kids = []
+    items.sort()
     for item in items:
         print(item)
 if __name__ == "__main__":
