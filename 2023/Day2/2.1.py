6ca7ddd77db0797b1ae17787472ccd3b08b093ba
Ben <benjamindwheeler@gmail.com>
2023-12-02 16:14:38 +1030

    day 2 part 1 done

--- 2023/Day2/2.1.py
+++ 2023/Day2/2.1.py
@@ -1,25 +1,44 @@
+map = {
+' red': 'r',
+' blue': 'b',
+' green': 'g',
+}
+
 with open('input.txt', 'r') as file:
     lines = []
+    total = 0
 
     for line in file:
         line = line.strip()
-        currentLine = []
 
-        for char in line: 
-            if char.isdigit():
-                if len(currentLine) > 1:
-                    currentLine[1] = char
+        numberWords = map.keys()
+        for word in numberWords:
+            line = line.replace(word, map[word])
+        words = line.split()
+        invalid = False
+        id = 0
+        for word in words:
+            if "Game" not in word:
+                if ":" in word:
+                    word = word.replace(":", "")
+                    id = int(word)
                 else:
-                    currentLine.append(char)
-                    currentLine.append(char)
-
-                # print(char)
-
-        lines.append(currentLine)
-
-    total = 0
-    numbers = [int(''.join(pair)) for pair in lines]
-    for number in numbers:
-        total += number
+                    if id != 0:
+                        word = word.replace(",", "")
+                        word = word.replace(";", "")
+                        letter = word[-1]
+                        number = int(''.join(c for c in word if c.isdigit()))
+                        if letter == 'b':
+                            if number > 14:
+                                invalid = True
+                        if letter == 'r':
+                            if number > 12:
+                                invalid = True
+                        if letter == 'g':
+                            if number > 13:
+                                invalid = True
+        if invalid == False:
+            total += id
+        # print(total)
 
 print(total)
