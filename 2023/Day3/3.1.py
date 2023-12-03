21da832365824537fd4b04a3191e5365ffe7d7cb
Ben <92675290+ben-wheeler@users.noreply.github.com>
2023-12-03 19:49:28 +1030

    delete extra prints

--- 2023/Day3/3.1.py
+++ 2023/Day3/3.1.py
@@ -8,7 +8,6 @@ with open('input.txt', 'r') as file:
         currentSymbols = []
 
         for char in line: 
-            # currentLine.append(char)
             if not char.isdigit():
                 currentLine.append('.')
                 if char != ".":
@@ -20,33 +19,23 @@ with open('input.txt', 'r') as file:
                 currentSymbols.append('F')
         lines.append(currentLine)
         symbols.append(currentSymbols)
-        # print(currentLine)
-        # print(currentSymbols)
 
     rows = len(lines)
     cols = len(lines[0])
-    # print(lines)
-    print(rows)
-    print(cols)
     partNumbers = []
 
     for i in range(rows):
         currentNumber = ""
         for j in range(cols):
             char = lines[i][j]
-            print(f"at {i} {j}")
             if char.isdigit():
-                print("number")
                 currentNumber += char
                 if j == cols-1:
-                    print("last in row")
                     if len(currentNumber) > 0:
                         number = int(currentNumber)
                         for row in range(max(0, i-1), min(rows, i+2)):
                             for col in range(max(0, j-len(currentNumber)-1), min(cols, j+1)):
-                                print(f"checking[{row}][{col}]")
                                 if symbols[row][col] == 'T':
-                                    print("found!")
                                     partNumbers.append(number)
                                     number = 0
                     currentNumber = ""
@@ -55,12 +44,9 @@ with open('input.txt', 'r') as file:
                     number = int(currentNumber)
                     for row in range(max(0, i-1), min(rows, i+2)):
                         for col in range(max(0, j-len(currentNumber)-1), min(cols, j+1)):
-                            print(f"checking[{row}][{col}]")
                             if symbols[row][col] == 'T':
-                                print("found!")
                                 partNumbers.append(number)
                                 number = 0
                 currentNumber = ""
                     
-print(partNumbers)
 print(sum(partNumbers))
