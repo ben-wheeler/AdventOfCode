6c245ed62270f8a8a3360702d26d2de3bc033713
Ben <92675290+ben-wheeler@users.noreply.github.com>
2023-12-04 18:00:01 +1030

    faster (it remembers)

--- 2023/Day4/4.2.py
+++ 2023/Day4/4.2.py
@@ -1,5 +1,3 @@
-
-
 with open('input.txt', 'r') as file:
     lines = []
     points = []
@@ -10,31 +8,34 @@ with open('input.txt', 'r') as file:
     totalCards = 0
 
     for line_number, line in enumerate(file, start=0):
-        print(line_number)
+        # print(line_number)
         for i in range(cardCount[line_number]):
-            line = line.strip()
-
-            line = line.replace("Card", "")
-            section = 0
-            words = line.split()
-            game = 0
-            winningCards = []
             howManyAmIWinning = 0
-            # print(line)
-            for word in words:
-                if ":" in word:
-                    # this is the card number
-                    section = 1
-                    word = word.replace(":", "")
-                    game = int(word)
-                elif "|" in word:
-                    section = 2
-                elif section == 1:
-                    winningCards.append(int(word))
-                elif section == 2:
-                    if int(word) in winningCards:
-                        howManyAmIWinning += 1
-            # print(f"g{game} u won{howManyAmIWinning}")
+            if i == 0:
+                line = line.strip()
+
+                line = line.replace("Card", "")
+                section = 0
+                words = line.split()
+                game = 0
+                winningCards = []
+                # print(line)
+                for word in words:
+                    if ":" in word:
+                        # this is the card number
+                        section = 1
+                        word = word.replace(":", "")
+                        game = int(word)
+                    elif "|" in word:
+                        section = 2
+                    elif section == 1:
+                        winningCards.append(int(word))
+                    elif section == 2:
+                        if int(word) in winningCards:
+                            howManyAmIWinning += 1
+                winsForRound[line_number] = howManyAmIWinning
+            else:
+                howManyAmIWinning = winsForRound[line_number]
             for i in range(howManyAmIWinning):
                 if game + i < len(cardCount):
                     cardCount[game + i] += 1
