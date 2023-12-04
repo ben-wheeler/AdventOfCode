1cd0e86e0f3f640eaed740d878a32f1f9d7ffd56
Ben <92675290+ben-wheeler@users.noreply.github.com>
2023-12-04 17:56:20 +1030

    part 2 done

--- 2023/Day4/4.2.py
+++ 2023/Day4/4.2.py
@@ -1,34 +1,43 @@
+
+
 with open('input.txt', 'r') as file:
     lines = []
     points = []
+    game_count = len(file.readlines())
+    file.seek(0)
+    cardCount = [1] * game_count
+    winsForRound = [-1] * game_count
+    totalCards = 0
 
-    for line in file:
-        line = line.strip()
+    for line_number, line in enumerate(file, start=0):
+        print(line_number)
+        for i in range(cardCount[line_number]):
+            line = line.strip()
 
-        line = line.replace("Card", "")
-        section = 0
-        words = line.split()
-        game = 0
-        winningCards = []
-        howManyAmIWinning = 0
-        # print(line)
-        for word in words:
-            if ":" in word:
-                # this is the card number
-                section = 1
-                game = word
-            elif "|" in word:
-                section = 2
-            elif section == 1:
-                winningCards.append(int(word))
-            elif section == 2:
-                if int(word) in winningCards:
-                    howManyAmIWinning += 1
+            line = line.replace("Card", "")
+            section = 0
+            words = line.split()
+            game = 0
+            winningCards = []
+            howManyAmIWinning = 0
+            # print(line)
+            for word in words:
+                if ":" in word:
+                    # this is the card number
+                    section = 1
+                    word = word.replace(":", "")
+                    game = int(word)
+                elif "|" in word:
+                    section = 2
+                elif section == 1:
+                    winningCards.append(int(word))
+                elif section == 2:
+                    if int(word) in winningCards:
+                        howManyAmIWinning += 1
+            # print(f"g{game} u won{howManyAmIWinning}")
+            for i in range(howManyAmIWinning):
+                if game + i < len(cardCount):
+                    cardCount[game + i] += 1
+                    totalCards += 1
 
-        print(f"{game} {howManyAmIWinning}")
-        pointsThisRound = 0
-        if(howManyAmIWinning > 0):
-            pointsThisRound = 2**(howManyAmIWinning-1)
-        points.append(pointsThisRound)
-
-print(sum(points))
+print(totalCards+game_count)
